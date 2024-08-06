8595fd5d126a6c5c60e65c978c85f009fbb90c1b
Putri Mila <putrimiila.fx@gmail.com>
2024-08-06 09:40:43 +0800

    arrow function lanjutan

--- 05.ArrowFunction/script.js
+++ 05.ArrowFunction/script.js
@@ -24,7 +24,7 @@
 // const tampilNama = () => 'Hello world';
 // console.log(tampilNama());
 
-let mahasiswa = ['Putri', 'Mila', 'Jiji', 'Herman'];
+// let mahasiswa = ['Putri', 'Mila', 'Jiji', 'Herman'];
 
 // let jumlahHuruf = mahasiswa.map(function (nama) {
 //     return nama.length;
@@ -37,5 +37,76 @@ let mahasiswa = ['Putri', 'Mila', 'Jiji', 'Herman'];
 
 // console.log(jumlahHuruf);
 
-let jumlahHuruf = mahasiswa.map(nama => ({ nama, jumlahHuruf: nama.length }));
-console.table(jumlahHuruf);
+// let jumlahHuruf = mahasiswa.map(nama => ({ nama, jumlahHuruf: nama.length }));
+// console.table(jumlahHuruf);
+
+// Konsep this pada Arrow function
+// Constructor function
+// const Mahasiswa = function() {
+//     this.nama = 'Putri';
+//     this.umur = 32;
+//     this.sayHello = function () {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const Putri = new Mahasiswa();
+
+// Arrow function
+// const Mahasiswa = function() {
+//     this.nama = 'Putri';
+//     this.umur = 32;
+//     this.sayHello = () => {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const Putri = new Mahasiswa();
+
+// Object literal
+// const mhs1 = {
+//     nama: 'Putri',
+//     umur: 32,
+//     sayHello: () => {
+//         // console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//         console.log(this);
+//     }
+// }
+
+// const Mahasiswa = function() {
+//     this.nama = 'Putri';
+//     this.umur = 32;
+//     this.sayHello =  function () {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     }, 500);
+// }
+
+// const Putri = new Mahasiswa();
+
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function() {
+    let satu = 'size';
+    let dua = 'caption';
+
+    if(this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
+    }
+
+    this.classList.toggle('size');
+    setTimeout(() => {
+        this.classList.toggle('caption');
+    }, 600);
+})
+
+
+
+
+
+
+
+
